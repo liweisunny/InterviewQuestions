7e3612bdebf7d716d03421d043b588c141a58695
liwei1 <liwei1@corp.beisencorp.com>
2018-05-20 15:29:54 +0800

    liwei

--- 5.排序算法/test.py
+++ 5.排序算法/test.py
@@ -100,7 +100,92 @@ def partition(lst, low, high):
     return low
 
 
+def insert_sort(lst):
 
-lst = [3, 2, 4, 5, 1, 4]
-quick_sort2(lst)
-print(lst)  # [1, 2, 3, 4, 4, 5]
+    i = 1
+    while i < len(lst):
+
+        j = i-1
+        insert_item = lst[i]
+
+        while j >= 0 and lst[j] > insert_item:
+            lst[j+1] = lst[j]
+            j -= 1
+
+        lst[j+1] = insert_item  # j+1 为当前带插入的位置，因为在找到带插入位置后会经过 j-=1。
+
+        i += 1
+
+
+def shell_sort(lst):
+    n = len(lst)
+    gap = n//2  # 初始步长
+    while gap > 0:
+
+        # 开始插入排序
+        i = gap  # 直接插入排序的起始元素是第二个元素，希尔排序的起始元素是索引为当前步长的那个元素
+        while i <= n-gap:  # 循环
+
+            j = i - gap
+            insert_item = lst[i]
+
+            while j >= 0 and lst[j] > insert_item:
+                lst[j+gap] = lst[j]
+                j -= gap
+
+            lst[j+gap] = insert_item
+            i += 1
+
+        gap //= 2
+
+
+def heap_sort(lst):
+    length = len(lst)
+    # 初始化堆
+    i = length//2
+    while i >= 0:
+        init_heap(lst, i, length)
+        i -= 1
+
+    # 执行堆排序过程
+    j = length - 1
+    while j > 0:
+        lst[j], lst[0] = lst[0], lst[j]
+        # 筛选 R[0] 结点，得到i-1个结点的堆
+        init_heap(lst, 0, j)
+        j -= 1
+
+
+def init_heap(lst, parent, length):
+    temp = lst[parent]  # 保存当前父节点
+    child = 2 * parent + 1  # 先获得左孩子
+    while child < length:
+
+        if child + 1 < length and lst[child] < lst[child + 1]:  # 如果有右孩子结点，并且右孩子结点的值大于左孩子结点，则选取右孩子结点
+            child += 1
+
+        if temp >= lst[child]:  # 如果父结点的值已经大于孩子结点的值，则直接结束
+            break
+
+        lst[parent] = lst[child]  # 把孩子结点的值赋给父结点
+
+        parent = child  # 选取孩子结点的左孩子结点, 继续向下筛选
+        child = 2 * child + 1
+    lst[parent] = temp
+
+lst = [3, 2, 4, 5, 1, 4, 7]
+heap_sort(lst)
+print(lst)  # [1, 2, 3, 4, 4, 5, 7]
+
+
+# def insertSort(relist):
+#     len_ = len(relist)
+#     for i in range(1,len_):
+#         for j in range(i):
+#             if relist[i] < relist[j]:
+#                 relist.insert(j,relist[i])  # 首先碰到第一个比自己大的数字，赶紧刹车，停在那，所以选择insert
+#                 relist.pop(i+1)  # 因为前面的insert操作，所以后面位数+1，这个位置的数已经insert到前面去了，所以pop弹出
+#                 break
+#     return relist
+#
+# print(insertSort([1,5,2,6,9,3]))
--- 5.排序算法/排序算法.py
+++ 5.排序算法/排序算法.py
@@ -3,23 +3,64 @@
 
 import random
 
+
 def swap(lst, i, j):
     lst[i], lst[j] = lst[j], lst[i]
 
 
 # 选择排序
-def selectSort(lyst):
+def select_sort(lst):
     i = 0
-    while i < len(lyst)-1:
-        min_index = i
+    while i < len(lst)-1:
+
         j = i+1
-        while j < len(lyst):
-            if lyst[j] < lyst[min_index]:
+        min_index = i
+
+        while j < len(lst):
+            if lst[min_index] > lst[j]:
                 min_index = j
             j += 1
+
         if min_index != i:
-            swap(lyst, min_index, i)
+            lst[min_index], lst[i] = lst[i], lst[min_index]
+
         i += 1
+
+
+# 堆排序
+def heap_sort(lst):
+    length = len(lst)
+    # 初始化堆
+    i = length // 2
+    while i >= 0:
+        init_heap(lst, i, length)
+        i -= 1
+
+    # 执行堆排序过程
+    j = length - 1
+    while j > 0:
+        lst[j], lst[0] = lst[0], lst[j]
+        # 筛选 R[0] 结点，得到i-1个结点的堆
+        init_heap(lst, 0, j)
+        j -= 1
+
+
+def init_heap(lst, parent, length):
+    temp = lst[parent]  # 保存当前父节点
+    child = 2 * parent + 1  # 先获得左孩子
+    while child < length:
+
+        if child + 1 < length and lst[child] < lst[child + 1]:  # 如果有右孩子结点，并且右孩子结点的值大于左孩子结点，则选取右孩子结点
+            child += 1
+
+        if temp >= lst[child]:  # 如果父结点的值已经大于孩子结点的值，则直接结束
+            break
+
+        lst[parent] = lst[child]  # 把孩子结点的值赋给父结点
+
+        parent = child  # 选取孩子结点的左孩子结点, 继续向下筛选
+        child = 2 * child + 1
+    lst[parent] = temp
 
 
 # 冒泡排序
@@ -41,64 +82,82 @@ def bubble_sort(lst):
         i += 1
 
 
+# 快速排序
+def quick_sort(lst):
+    quick_sort_helper(lst, 0, len(lst) - 1)
+
+
+def quick_sort_helper(lst, low, high):
+    if low >= high:
+        return
+    # three_standard_value(lst, low, high)
+    middle_index = partition(lst, low, high)
+    quick_sort_helper(lst, low, middle_index - 1)
+    quick_sort_helper(lst, middle_index + 1, high)
+
+
+def three_standard_value(lst, low, high):
+
+    ''' 三数取中确定基准元，将确定好的基准元与第一个数交换'''
+    mid = (high + low) // 2
+
+    if lst[mid] > lst[high]:
+        lst[mid], lst[high] = lst[high], lst[mid]
+
+    if lst[low] > lst[high]:
+        lst[low], lst[high] = lst[high], lst[low]
+
+    if lst[mid] > lst[low]:
+        lst[mid], lst[low] = lst[low], lst[mid]
+
+
+def partition(lst, low, high):
+    key = lst[low]  # 取第一个数为基准值
+    while low < high:
+        while low < high and lst[high] >= key:  # 从列表右边开始把小于基准值的元素放到
+            high -= 1
+        lst[low] = lst[high]
+        while low < high and lst[low] <= key:
+            low += 1
+        lst[high] = lst[low]
+    lst[low] = key
+    return low
+
+
 # 插入排序
-def insertionSort(lyst):
+def insert_sort(lst):
     i = 1
-    count = 0
-    while i < len(lyst):
-        itemToInsert = lyst[i]
+    while i < len(lst):
         j = i-1
-        while j >= 0:
-            count += 1
-            if itemToInsert < lyst[j]:
-                lyst[j+1] = lyst[j]
-                j -= 1
-            else:
-                break
-        lyst[j+1] = itemToInsert
+        insert_item = lst[i]
+        while j >= 0 and lst[j] > insert_item:
+            lst[j+1] = lst[j]
+            j -= 1
+        lst[j+1] = insert_item # j+1 为当前带插入的位置，因为在找到带插入位置后会经过 j-=1。
         i += 1
-    print(count)
 
 
-# 快速排序1
-def quickSort(lyst):
-    if len(lyst) <= 1:
-        return lyst
-    middleIndx = len(lyst)//2
-    middleValue = lyst[middleIndx]
+# 希尔排序
+def shell_sort(lst):
+    n = len(lst)
+    gap = n // 2  # 初始步长
+    while gap > 0:
 
-    left = [i for i in lyst if middleValue > i]
-    right = [i for i in lyst if middleValue < i]
+        # 开始插入排序
+        i = gap  # 直接插入排序的起始元素是第二个元素，希尔排序的起始元素是索引为当前步长的那个元素
+        while i <= n - gap:  # 循环
 
-    return quickSort(left) + [middleValue] * lyst.count(middleValue) + quickSort(right)
+            j = i - gap
+            insert_item = lst[i]
 
+            while j >= 0 and lst[j] > insert_item:
+                lst[j + gap] = lst[j]
+                j -= gap
 
-# 快速排序2
-def quickSort2(lyst):
-    quicksortHelper(lyst, 0, len(lyst) - 1)
+            lst[j + gap] = insert_item
+            i += 1
 
-
-def quicksortHelper(lyst, left, right):
-    if left < right:
-        pivot_location = partition(lyst, left, right)
-        quicksortHelper(lyst, left, pivot_location - 1)
-        quicksortHelper(lyst, pivot_location + 1, right)
-
-
-
-
-def partition(lyst, left, right):
-    middle = (left + right)//2  # left + (right-left)/2
-    pivot = lyst[middle]
-    lyst[middle] = lyst[right]
-    lyst[right] = pivot
-    boundry = left
-    for index in range(left, right):
-        if lyst[index] < pivot:
-            swap(lyst, index, boundry)
-            boundry += 1
-    swap(lyst, right, boundry)
-    return boundry
+        gap //= 2
 
 
 # 归并排序
